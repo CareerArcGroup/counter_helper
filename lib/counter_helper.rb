02b6f40ea35ea9097b2b0e900cbacbc3db81c89a
Josh Ofrane <jofrane@careerarc.com>
2018-09-07 15:48:35 -0700

    sync gem to current ca codebase

--- lib/counter_helper.rb
+++ lib/counter_helper.rb
@@ -8,14 +8,18 @@ module CounterHelper
     include Redis::RedisHelper
 
     COUNTER_LIST_KEY = "counter_helper:counter_list"
+    AVERAGE_LIST_KEY = "counter_helper:average_list"
     COUNTER_LIST_LOCK_KEY = "counter_helper:counter_list_lock"
+    AVERAGE_LIST_LOCK_KEY = "counter_helper:average_list_lock"
     COUNTER_SLICE_PREFIX = "counter_helper:counters"
+    MAX_KEY_LENGTH = 255
 
     # =================================================================
     # counter operations
     # =================================================================
 
     def increment(key, by=1, &block)
+      key = normalize_key(key)
       register(key) unless registered?(key)
       slice = slice_counter(key)
       value = slice.increment(by)
@@ -24,11 +28,37 @@ module CounterHelper
     end
 
     def decrement(key, by=1, &block)
+      key = normalize_key(key)
       register(key) unless registered?(key)
       slice = slice_counter(key)
       value = slice.decrement(by)
 
       block_given? ? rewindable_block(:increment, key, by, value, &block) : value
+    end
+
+    def push(key, value)
+      key = normalize_key(key)
+      register(key, true) unless registered?(key)
+      ss = Redis::SortedSet.new(key, redis)
+      ss.add(value, Time.now.to_i)
+    end
+
+    def average_data(key)
+      key = normalize_key(key)
+      window = 100
+      ss = Redis::SortedSet.new(key, redis)
+
+      nums = ss.rev_range(0, window)
+      if ss.length > window
+        limit = ss.score(ss.at(window * -1))
+        ss.delete_if!{|x| ss.score(x) < limit}
+      end
+      nums
+    end
+
+    def average(key)
+      nums = average_data(key)
+      nums.empty? ? 0.0 : nums.map(&:to_f).reduce(0, :+) / nums.count
     end
 
     def increment_with_logging(key, message, options = {}, &block)
@@ -44,10 +74,12 @@ module CounterHelper
     end
 
     def value(key)
+      key = normalize_key(key)
       slice_counter(key).value
     end
 
     def has_counter?(key)
+      key = normalize_key(key)
       registered?(key)
     end
 
@@ -72,6 +104,39 @@ module CounterHelper
       read_counter(key, true, &block)
     end
 
+    def read_averages!(&block)
+      average_list_lock.lock do
+        each_average do |key|
+          yield ({ average: key.first, value: average(key.first) })
+        end
+      end
+    end
+
+    def prune_counters
+      keep_count = 0
+      prune_count = 0
+      end_slice = slice_index - 1
+      each_counter do |key, last_read_slice|
+        has_data = false
+        each_counter_value(key, 0, end_slice, false) do |data|
+          if data[:value] > 0
+            has_data = true
+            break
+          end
+        end
+
+        if has_data
+          keep_count += 1
+        else
+          prune_count += 1
+          puts "Counter '#{key}' does NOT have data; unregistering"
+          unregister(key)
+        end
+      end
+
+      puts "Done! Examined #{keep_count + prune_count} counters, #{keep_count} have data, #{prune_count} do not and have been unregistered."
+    end
+
     def configure(options={})
       Config.from_options(options)
     end
@@ -82,8 +147,16 @@ module CounterHelper
       @counter_list ||= Redis::SortedSet.new(COUNTER_LIST_KEY, redis)
     end
 
+    def average_list
+      @average_list ||= Redis::SortedSet.new(AVERAGE_LIST_KEY, redis)
+    end
+
     def counter_list_lock
       @counter_list_lock ||= Redis::Lock.new(COUNTER_LIST_LOCK_KEY, redis, timeout: 0, expiration: 60)
+    end
+
+    def average_list_lock
+      @average_list_lock ||= Redis::Lock.new(AVERAGE_LIST_LOCK_KEY, redis, timeout: 0, expiration: 60)
     end
 
     # =================================================================
@@ -92,18 +165,18 @@ module CounterHelper
 
     # adds the counter key to the counter list
     # with a score of 0...
-    def register(key)
-      counter_list.add(key, slice_index - 1)
+    def register(key, average = false)
+      average ? average_list.add(key, slice_index - 1) : counter_list.add(key, slice_index - 1)
     end
 
     # removes the counter key from the counter list
-    def unregister(key)
-      counter_list.delete!(key)
+    def unregister(key, average = false)
+      average ? average_list.delete!(key) : counter_list.delete!(key)
     end
 
     # checks to see if the counter is registered
-    def registered?(key)
-      counter_list.score(key) != nil
+    def registered?(key, average = false)
+      average ? average_list.score(key) != nil : counter_list.score(key) != nil
     end
 
     # =================================================================
@@ -143,6 +216,10 @@ module CounterHelper
     # enumerate over counters with their last-viewed times...
     def each_counter(&block)
       counter_list.members(with_scores: true).map(&block)
+    end
+
+    def each_average(&block)
+      average_list.members(with_scores: true).each(&block); nil
     end
 
     # enumerates over counter values for the specific key
@@ -250,6 +327,20 @@ module CounterHelper
     # misc
     # =================================================================
 
+    # normalize the given key. the only normalization that we
+    # do at this point is to make sure the key isn't too long...
+    def normalize_key(key)
+      return key unless key && key.length > MAX_KEY_LENGTH
+
+      original_key = key.dup
+      key = key[0...MAX_KEY_LENGTH]
+
+      # increment a counter indicating the problem...
+      increment_with_logging("counter_helper:key_normalization", "Counter key required normaliation", _original_key: original_key, _normalized_key: key)
+
+      key
+    end
+
     # performs a "rewind" operation after an increment or decrement
     # operation (returning the counter to it's previous value) after the
     # execution of the given block. see the increment and decrement
@@ -271,7 +362,7 @@ module CounterHelper
     end
 
     def key_with_prefix(key)
-      [Config.redis_prefix, key].join(":")
+      [Config.redis_prefix, key].compact.join(":")
     end
   end
 end
